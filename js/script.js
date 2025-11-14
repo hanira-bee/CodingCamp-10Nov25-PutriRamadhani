16ddb12cc1b6372da70ad4a7de9c33a6b7e2f51f
hanira-bee <rinnieera@gmail.com>
2025-11-14 23:49:17 +0700

    add task rendering functionality and handle empty task list

--- js/script.js
+++ js/script.js
@@ -4,5 +4,24 @@ document.addEventListener('DOMContentLoaded', () => {
     const taskInput = document.getElementById('task-input');
     const dueDateInput = document.getElementById('due-date-input');
     const addTaskBtn = document.getElementById('add-task-btn)');
+    const tasksBody = document.getElementById('tasks-body');
+    const deleteAllBtn = document.getElementById('delete-all-btn');
+    const filterBtn = document.getElementById('filter-btn');
+    const filterSelect = document.getElementById('filter-select');
+
+    function renderTasks() {
+        tasksBody.innerHTML = '';
+
+        if (tasks.length === 0) {
+            const row = document.createElement('tr');
+            row.className = 'no-task-row';
+            let td = document.createElement('td');
+            td.colSpan = 4;
+            td.textContent = 'No taks found';
+            row.appendChild(td);
+            tasksBody.appendChild(row);
+            return;
+        }
+    }
 
 })
