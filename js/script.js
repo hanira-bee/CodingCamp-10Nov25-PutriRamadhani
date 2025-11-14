4239afc764c3573d9d43e29c305a020d86a23330
hanira-bee <rinnieera@gmail.com>
2025-11-14 23:40:55 +0700

    css and js

---
+++ js/script.js
@@ -0,0 +1,8 @@
+let todos = []
+
+document.addEventListener('DOMContentLoaded', () => {
+    const taskInput = document.getElementById('task-input');
+    const dueDateInput = document.getElementById('due-date-input');
+    const addTaskBtn = document.getElementById('add-task-btn)');
+
+})
